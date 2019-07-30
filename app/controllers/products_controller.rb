74c0fae01e3b1148d5f11cfc4fa7aa0a8b390159
Brian <briandsalemi@gmail.com>
2019-07-30 08:12:43 -0400

    Done.

---
+++ app/controllers/products_controller.rb
@@ -0,0 +1,19 @@
+require 'pry'
+class ProductsController < ApplicationController 
+     
+
+
+    def index
+    end 
+
+    def add
+        product = params[:product]
+        cart << product 
+        render :index 
+    end 
+
+
+
+
+
+end 
